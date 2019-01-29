eab91983377db3a887e3da16ea02aa1b32f8a58a
Axel Boström <axel.bostrom@interactivesolutions.se>
2019-01-29 16:16:19 +0100

    Updated to be compatible with zf3

--- src/InteractiveSolutions/Stdlib/Factory/Mvc/Controller/Plugin/ValidateIncomingDataFactory.php
+++ src/InteractiveSolutions/Stdlib/Factory/Mvc/Controller/Plugin/ValidateIncomingDataFactory.php
@@ -10,14 +10,16 @@ declare(strict_types = 1);
 namespace InteractiveSolutions\Stdlib\Factory\Mvc\Controller\Plugin;
 
 use InteractiveSolutions\Stdlib\Mvc\Controller\Plugin\ValidateIncomingData;
+use Psr\Container\ContainerInterface;
+use Zend\InputFilter\InputFilterPluginManager;
 use Zend\Mvc\Controller\PluginManager;
 
 final class ValidateIncomingDataFactory
 {
-    public function __invoke(PluginManager $pluginManager)
+    public function __invoke(ContainerInterface $container)
     {
         return new ValidateIncomingData(
-            $pluginManager->getServiceLocator()->get('inputFilterManager')
+            $container->get(InputFilterPluginManager::class)
         );
     }
 }
